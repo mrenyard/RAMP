ba9b4ce9580d7f38bb5b7fcb7efeb53f069f3e47
mrenyard <renyard.m@gmail.com>
2020-05-25 22:14:52 +0100

    Add DateTime db type validation rules

---
+++ code/model/business/validation/dbtype/Date.class.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Svelte - Rapid web application development using best practice.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @author Matt Renyard (renyard.m@gmail.com)
+ * @version 0.0.9;
+ */
+namespace svelte\model\business\validation\dbtype;
+
+use svelte\core\Str;
+use svelte\model\business\FailedValidationException;
+use svelte\model\business\validation\ValidationRule;
+
+/**
+ * Date database type validation rule, in the format YYYY-mm-dd.
+ * Runs code defined test against provided value.
+ */
+class Date extends DbTypeValidation
+{
+  /**
+   * Default constructor for a validation rule of database type Date.
+   * Multiple ValidationRules can be wrapped within each other to form a more complex set of tests:
+   * ```php
+   * $myValidationRule = new validation\dbtype\Date(
+   *   new validation\SecondValidationRule(
+   *     new validation\ThirdValiationRule(
+   *       new validation\ForthValidationRule()
+   *     )
+   *   ),
+   *   Str::set('My error message HERE!')
+   * );
+   * ```
+   * @param \svelte\model\business\validation\ValidationRule $subRule Addtional rule/s to be added
+   * @param \svelte\core\Str $errorMessage Message to be displayed when tests unsuccessful
+   */
+  public function __construct(ValidationRule $subRule, Str $errorMessage)
+  {
+    parent::__construct($subRule, $errorMessage);
+  }
+
+  /**
+   * Asserts that $value is a valid date in the format YYYY-MM-DD.
+   * @param mixed $value Value to be tested.
+   * @throws FailedValidationException When test fails.
+   */
+  protected function test($value)
+  {
+    $format = 'Y-m-d';
+    $o = \DateTime::createFromFormat($format, $value);
+    if ($o && $o->format($format) === $value) { return; }
+    throw new FailedValidationException();
+  }
+}
---
+++ tests/model/business/validation/dbtype/DateTimeTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Testing - Svelte - Rapid web application development using best practice.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @author Matt Renyard (renyard.m@gmail.com)
+ * @version 0.0.9;
+ */
+namespace tests\svelte\model\business\dbtype\validation;
+
+require_once '/usr/share/php/svelte/core/SvelteObject.class.php';
+require_once '/usr/share/php/svelte/model/business/FailedValidationException.class.php';
+require_once '/usr/share/php/svelte/model/business/validation/ValidationRule.class.php';
+require_once '/usr/share/php/svelte/model/business/validation/dbtype/DbTypeValidation.class.php';
+require_once '/usr/share/php/svelte/model/business/validation/dbtype/DateTime.class.php';
+
+require_once '/usr/share/php/tests/svelte/model/business/validation/mocks/ValidationRuleTest/FailOnBadValidationRule.class.php';
+
+use svelte\core\Str;
+use svelte\model\business\FailedValidationException;
+use svelte\model\business\validation\dbtype\DateTime;
+
+use tests\svelte\model\business\validation\FailOnBadValidationRule;
+
+/**
+ * Collection of tests for \svelte\model\business\validation\dbtype\DateTime.
+ */
+class DateTimeTest extends \PHPUnit\Framework\TestCase
+{
+  private $testObject;
+  private $errorMessage;
+
+  /**
+   * Setup
+   */
+  public function setUp()
+  {
+    $this->maxLength = 10;
+    $this->errorMessage = Str::set('My error message HERE!');
+    $this->testObject = new DateTime(
+      new FailOnBadValidationRule(),
+      $this->errorMessage
+    );
+  }
+
+  /**
+   * Collection of assertions for svelte\validation\dbtype\DateTime::__construct().
+   * - assert is instance of {@link \svelte\core\SvelteObject}
+   * - assert is instance of {@link \svelte\model\business\validation\ValidationRule}
+   * - assert is instance of {@link \svelte\model\business\validation\DateTime}
+   * @link svelte.model.business.validation.dbtype.DateTime \svelte\model\business\validation\dbtype\DateTime
+   */
+  public function test__Construct()
+  {
+    $this->assertInstanceOf('svelte\core\SvelteObject', $this->testObject);
+    $this->assertInstanceOf('svelte\model\business\validation\ValidationRule', $this->testObject);
+    $this->assertInstanceOf('svelte\model\business\validation\dbtype\DbTypeValidation', $this->testObject);
+    $this->assertInstanceOf('svelte\model\business\validation\dbtype\DateTime', $this->testObject);
+  }
+
+  /**
+   * Collection of assertions for svelte\model\business\validation\dbtype\DateTime::process().
+   * - assert void returned when test successful
+   * - assert {@link \svelte\model\business\FailedValidationException} thrown when test fails
+   * @link svelte.model.business.validation.dbtype.DateTime#method_process \svelte\model\business\validation\dbtype\DateTime::process()
+   */
+  public function testTest()
+  {
+    $this->assertNull($this->testObject->process('2006-12-24T23:29:59'));
+    try {
+      $this->testObject->process('2006-12-24T24:00:00'); // Shoud be 2006-12-24T00:00:00.
+    } catch (FailedValidationException $expected) {
+      $this->assertEquals((string)$this->errorMessage, $expected->getMessage());
+      return;
+    }
+    $this->fail('An expected \svelte\model\business\FailedValidationException has NOT been raised.');
+  }
+}
