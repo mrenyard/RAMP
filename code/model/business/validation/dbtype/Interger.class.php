7024a54dfa7c7c3dbf73574bc60c1c2a38883d97
mrenyard <renyard.m@gmail.com>
2020-05-25 18:21:32 +0100

    Add validation db type Integer

---
+++ code/model/business/validation/dbtype/Interger.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Svelte - Rapid web application development using best practice.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @author Matt Renyard (renyard.m@gmail.com)
+ * @version 0.0.9;
+ */
+namespace svelte\model\business\validation\dbtype;
+
+use svelte\core\Str;
+use svelte\model\business\FailedValidationException;
+use svelte\model\business\validation\ValidationRule;
+
+/**
+ * Interger database type validation rule, TRUE or FALSE.
+ * Runs code defined test against provided value.
+ */
+class Interger extends DbTypeValidation
+{
+  /**
+   * Default constructor for a validation rule of database type Interger.
+   * @param \svelte\core\Str $errorMessage Message to be displayed when tests unsuccessful
+   */
+  public function __construct(Str $errorMessage)
+  {
+    parent::__construct(NULL, $errorMessage);
+  }
+
+  /**
+   * Asserts that $value is a boolean, TRUE or FALSE.
+   * @param mixed $value Value to be tested.
+   * @throws FailedValidationException When test fails.
+   */
+  protected function test($value)
+  {
+    if (is_int($value)) { return; }
+    throw new FailedValidationException();
+  }
+}
