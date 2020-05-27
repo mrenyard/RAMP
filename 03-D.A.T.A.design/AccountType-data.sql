0b385205caf5570eb9efcb6146d2efceb2092ff9
mrenyard <renyard.m@gmail.com>
2020-05-27 01:07:57 +0100

    Add example data design

---
+++ 03-D.A.T.A.design/AccountType-data.sql
@@ -0,0 +1,10 @@
+--
+-- Data for table `AccountType`
+--
+INSERT INTO `AccountType` (`key`, `description`) VALUES
+(1, 'Registered'),
+(2, 'User'),
+(3, 'Affiliate'),
+(4, 'Administrator'),
+(5, 'Administrator Manager'),
+(6, 'System Administrator');
---
+++ 03-D.A.T.A.design/CountryList-data.sql
@@ -0,0 +1,35 @@
+--
+-- Data for table `CountryList`
+--
+INSERT INTO CountryList
+  (key, code, description, sageCode)
+ VALUES
+  (040, 'AT', 'AUSTRIA', 'T4'),
+  (056, 'BE', 'BELGIUM', 'T4'),
+  (191, 'HR', 'CROATIA', 'T9'),
+  (196, 'CY', 'CYPRUS', 'T4'),
+  (203, 'CZ', 'CZECH REPUBLIC', 'T4'),
+  (208, 'DK', 'DENMARK', 'T4'),
+  (233, 'EE', 'ESTONIA', 'T4'),
+  (246, 'FI', 'FINLAND', 'T4'),
+  (250, 'FR', 'FRANCE', 'T4'),
+  (276, 'DE', 'GERMANY', 'T4'),
+  (300, 'GR', 'GREECE', 'T4'),
+  (348, 'HU', 'HUNGARY', 'T4'),
+  (352, 'IS', 'ICELAND', 'T4'),
+  (372, 'IE', 'IRELAND', 'T4'),
+  (380, 'IT', 'ITALY', 'T4'),
+  (428, 'LV', 'LATVIA', 'T4'),
+  (440, 'LT', 'LITHUANIA', 'T4'),
+  (442, 'LU', 'LUXEMBOURG', 'T4'),
+  (470, 'MT', 'MALTA', 'T4'),
+  (528, 'NL', 'NETHERLANDS', 'T4'),
+  (616, 'PL', 'POLAND', 'T4'),
+  (703, 'SK', 'SLOVAKIA', 'T4'),
+  (705, 'SI', 'SLOVENIA', 'T4'),
+  (724, 'ES', 'SPAIN', 'T4'),
+  (752, 'SE', 'SWEDEN', 'T4'),
+  (792, 'TR', 'TURKEY', 'T9'),
+  (826, 'UK', 'UNITED KINGDOM', 'T1');
+
+-- --------------------------------------------------------
