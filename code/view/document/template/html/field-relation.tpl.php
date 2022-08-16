c5f140080c14c0fd0a9a3e63dd3a05b8b7e1180e
mrenyard <renyard.m@gmail.com>
2022-08-16 17:21:44 +0100

    .tpl modified inline with SelectFrom

--- code/view/document/template/html/field-relation.tpl.php
+++ code/view/document/template/html/field-relation.tpl.php
@@ -26,26 +26,22 @@ switch ($this->type) {
             <legend><?=$this->heading; ?></legend>
             <p class="note"><?=$this->summary; ?></p>
 <?php if ($this->hasErrors) { ?>
-            <ol class="error">
-<?php foreach ($this->errors as $error) { ?>            
-              <li><?=$error; ?></p>
-<?php } ?>
+            <ol class="error"><?php foreach ($this->errors as $error) { ?>            
+              <li><?=$error; ?></p><?php } ?>
             </ol>
 <?php } ?>
 <?=$this->children; ?>
           </fieldset><!-- #<?=$this->id; ?> -->
 <?php break; 
-  case 'select-one field': ?>
+  case 'select-one select-from': ?>
           <label class="<?=$this->type; ?>" for="<?=$this->id; ?>"<?=$this->attribute('title'); ?>>
             <span class="name"><?=$this->label; ?></span>
-            <select id="<?=$this->id; ?>" name="<?=$this->id; ?>" required="required" tabindex="<?=$tabindex; ?>">
-<?php foreach ($this->options as $option) { ?>            
-              <option value="<?=$option->key; ?>"<?=($option->isSelected)? ' selected':''; ?>><?=$option->description; ?></option>
-<?php } ?>
+            <select id="<?=$this->id; ?>" name="<?=$this->id; ?>" required="required" tabindex="<?=$tabindex; ?>"><?php foreach ($this->options as $option) { ?>            
+              <option value="<?=$option->key; ?>"<?=($option->isSelected)? ' selected':''; ?>><?=$option->description; ?></option><?php } ?>
+
             </select>
 <?php if ($this->hasErrors) { foreach ($this->errors as $error) { ?>            
-            <span class="error"><?=$error; ?></span>
-<?php } } ?>
+            <span class="error"><?=$error; ?></span><?php } } ?>
           </label>
 <?php break;
   case 'input field': ?>
@@ -53,8 +49,7 @@ switch ($this->type) {
             <span class="name"><?=$this->label; ?>:</span>
             <input id="<?=$this->id; ?>" name="<?=$this->id; ?>" type="text" placeholder="<?=$this->placeholder; ?>" value="<?=$this->value; ?>" tabindex="<?=$tabindex; ?>" />
 <?php if ($this->hasErrors) { foreach ($this->errors as $error) { ?>            
-            <span class="error"><?=$error; ?></span>
-<?php } } ?>
+            <span class="error"><?=$error; ?></span><?php } } ?>
           </label>
 <?php break;
 } ?>
