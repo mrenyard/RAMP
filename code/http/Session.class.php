fab528554cc8157cd430d1b5c1e6f32b2bb77719
mrenyard <renyard.m@gmail.com>
2024-12-14 15:19:25 +0000

    Add implicit inherited tests to ramp\http\Session

--- code/http/Session.class.php
+++ code/http/Session.class.php
@@ -305,6 +305,6 @@ final class Session extends RAMPObject
    */
   public function __clone()
   {
-    throw new \BadMethodCallException('Clone is not allowed');
+    throw new \BadMethodCallException('Cloning is not allowed');
   }
 }
